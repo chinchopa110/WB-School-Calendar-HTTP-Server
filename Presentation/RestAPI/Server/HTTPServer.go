170023f2eabb712bb276de6baa2108e298eefad9
polyakov <s_zhopa88@mail.ru>
2025-01-25 15:32:52 +0300

    fix: upd log service

--- Presentation/RestAPI/Server/HTTPServer.go
+++ Presentation/RestAPI/Server/HTTPServer.go
@@ -6,6 +6,7 @@ import (
 	"WB2/Presentation/RestAPI/Parser"
 	"WB2/Presentation/RestAPI/Parser/Factory"
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -23,25 +24,38 @@ func NewHTTPServer(userEventsRepo Repos.IUserEventsRepo) *HTTPServer {
 }
 
 func (s *HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Start handling request: %s %s", r.Method, r.URL.Path)
+
 	if s.parser == nil {
+		log.Println("Error: Parser is not initialized")
 		http.Error(w, "Parser is not initialized", http.StatusInternalServerError)
 		return
 	}
 
 	command, err := s.parser.TryParse(r)
 	if err != nil {
+		log.Printf("Error parsing request: %s\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	res := command.Execute()
-	if res.GetError() != nil {
+	if res == nil {
+		log.Println("Error: Response is nil")
+		http.Error(w, "Response is nil", http.StatusInternalServerError)
+		return
+	} else if res.GetError() != nil {
+		log.Printf("Error executing command: %s\n", res.GetError())
 		http.Error(w, res.GetError().Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Printf("Error encoding json: %s\n", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	log.Printf("End handling request: %s %s", r.Method, r.URL.Path)
 }
